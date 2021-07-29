37466ad77e4d356a01feb1e2150435ad721f1270
David Fan <fan.david@dhs.sg>
2021-07-29 15:01:09 +0800

    Web logic, Snowball Prefab, level2 spawning code
    Web onCollision disables after hitting player

--- GameDesignDraft/Assets/Scripts/Obstacle Scripts/RandomSpawner.cs
+++ GameDesignDraft/Assets/Scripts/Obstacle Scripts/RandomSpawner.cs
@@ -7,14 +7,14 @@ public class RandomSpawner : MonoBehaviour
     public Transform[] spawnPoints;
     public Transform spawnPoint;
     public GameObject[] obstaclePrefabs;
-    public GameObject treePrefab;
+    public GameObject[] treePrefabs;
     public float respawnTime;
     public Transform leftBound;
     public Transform rightBound;
     private CameraController cameraScript;
     public Camera mainCam;
     private bool oneTime = false;
-    private Vector3 prevObstaclePos = Vector3.zero;
+    public int level;
 
     // Start is called before the first frame update
     void Start()
@@ -31,8 +31,12 @@ public class RandomSpawner : MonoBehaviour
 
         if (div == 0) {
             if (!oneTime) {
-                // StartCoroutine(obstacleWave());
-                spawnObstacles();
+                if (level == 1) {
+                    spawnObstaclesLevel1();
+                } else if (level == 2) {
+                    spawnObstaclesLevel2();
+                }
+                
                 oneTime = true;
             }
         } else {
@@ -40,13 +44,12 @@ public class RandomSpawner : MonoBehaviour
         }
     }
 
-    private void spawnObstacles() {
+    private void spawnObstaclesLevel1() {
         // int numberOfObstacles = Random.Range(1, spawnPoints.Length);
 
         // Spawn a tree at random position
-
-        GameObject treeObstacle = Instantiate( treePrefab, new Vector3(spawnPoint.position.x + Random.Range(-1.0f, 1.0f), 0.89f, 0), Quaternion.identity);
-        // GameObject treeObstacle = Instantiate( treePrefab, new Vector3(prevObstaclePos.x + Random.Range(0.0f, 1.0f), 0.89f, 0), Quaternion.identity);
+        int randTree = Random.Range(0, treePrefabs.Length);
+        GameObject treeObstacle = Instantiate( treePrefabs[randTree], new Vector3(spawnPoint.position.x + Random.Range(-1.0f, 1.0f), 0.89f, 0), Quaternion.identity);
         // Then spawn others while checking the position of the tree
 
         // for (int j =  0; j  <  numberOfObstacles; j++) {
@@ -87,7 +90,6 @@ public class RandomSpawner : MonoBehaviour
             GameObject obstacle = Instantiate( obstaclePrefabs[randObstacle], randomPosition, Quaternion.identity);
             // obstacle.tag = "Spider";
             obstacle.layer = 9;
-            prevObstaclePos = obstacle.transform.position;
 
         }
 
@@ -111,10 +113,60 @@ public class RandomSpawner : MonoBehaviour
             GameObject obstacle = Instantiate( obstaclePrefabs[randObstacle], randomPosition, Quaternion.identity);
             // obstacle.tag = "Rock";
             obstacle.layer = 9;
-            prevObstaclePos = obstacle.transform.position;
         }
 
         // }
+    }
+
+    private void spawnObstaclesLevel2() {
+        int randnNumberOfObstacles = Random.Range(1, obstaclePrefabs.Length);
+
+        // Spawn a tree at random position
+        int randTree = Random.Range(0, treePrefabs.Length);
+        GameObject treeObstacle = Instantiate( treePrefabs[randTree], new Vector3(spawnPoint.position.x + Random.Range(-1.0f, 1.0f), 0.89f, 0), Quaternion.identity);
+        // GameObject treeObstacle = Instantiate( treePrefab, new Vector3(prevObstaclePos.x + Random.Range(0.0f, 1.0f), 0.89f, 0), Quaternion.identity);
+        // Then spawn others while checking the position of the tree
+
+        for (int j =  0; j  <  randnNumberOfObstacles; j++) {
+            
+            float leftRange;
+            if (treeObstacle.transform.position.x - 4.5f > leftBound.transform.position.x) {
+                leftRange = Random.Range(leftBound.transform.position.x, treeObstacle.transform.position.x - 4.5f);
+            } else {
+                leftRange = 0f;
+            }
+
+            float rightRange;
+            if (treeObstacle.transform.position.x + 3.0f < rightBound.transform.position.x) {
+                rightRange = Random.Range(treeObstacle.transform.position.x + 3.0f, rightBound.transform.position.x);
+            } else {
+                rightRange = 0f;
+            }
+
+            int randObstacle = Random.Range(0, obstaclePrefabs.Length);
+
+            if (obstaclePrefabs[randObstacle].name.Equals("Spider")) {
+                Debug.Log("Instantiating spider!");
+
+                float randX = Random.Range(leftBound.transform.position.x, rightBound.transform.position.x);
+                Vector3 randomPosition = new Vector3(randX, -3.12f, 0);
+
+                GameObject obstacle = Instantiate( obstaclePrefabs[randObstacle], randomPosition, Quaternion.identity);
+                obstacle.layer = 9;
+            }
+
+            if (obstaclePrefabs[randObstacle].name.Equals("Web1") || obstaclePrefabs[randObstacle].name.Equals("Web2") || obstaclePrefabs[randObstacle].name.Equals("Web3")) {
+                Debug.Log("Instantiating web!");
+
+                float randX = Random.Range(leftBound.transform.position.x, rightBound.transform.position.x);
+                float randY = Random.Range(-3, 4);
+                Vector3 randomPosition = new Vector3(randX, randY, 0);
+
+                GameObject obstacle = Instantiate( obstaclePrefabs[randObstacle], randomPosition, Quaternion.identity);
+                obstacle.layer = 9;
+            }
+
+        }
     }
 
     IEnumerator obstacleWave() {
